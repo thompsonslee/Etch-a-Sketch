8f785ef83528e66191d7555da41b7a9598d4f185
thompsonslee <thompson.slee@hotmail.co.uk>
2023-01-26 15:33:03 +1100

    change id to class for all divs

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const container = document.querySelector('#container');
+const container = document.querySelector('.container');
 for (let i =0 ; i < 16; i++){
     columndiv =document.createElement('div');
-    columndiv.setAttribute("id" , "column")
+    columndiv.setAttribute("class" , "column")
     for (let i = 0 ; i < 16 ; i++){
         rowdiv = document.createElement('div');
-        rowdiv.setAttribute("id", "row")
+        rowdiv.setAttribute("class", "row")
         columndiv.appendChild(rowdiv)
     }
     container.appendChild(columndiv)
