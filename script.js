f0968394300e3db2d99859c77a11774dfb7b8622
thompsonslee <thompson.slee@hotmail.co.uk>
2023-01-28 00:41:09 +1100

    add function to add class divs hovered over as "hovered"

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ const container = document.querySelector('.container');
 for (let i =0 ; i < 16; i++){
     columndiv =document.createElement('div');
     columndiv.setAttribute("class" , "column")
+    
     for (let i = 0 ; i < 16 ; i++){
-        rowdiv = document.createElement('div');
+        let rowdiv = document.createElement('div')
+        rowdiv.addEventListener("mouseover",
+        e => e.target.classList.add("hovered"))
         rowdiv.setAttribute("class", "row")
         columndiv.appendChild(rowdiv)
     }
