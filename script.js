b71477bd626c9ed54fd1d6ac6a3666d33f70d898
thompsonslee <thompson.slee@hotmail.co.uk>
2023-01-28 19:11:43 +1100

    change div generator to generate amount given by prompt

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 const container = document.querySelector('.container');
-for (let i =0 ; i < 16; i++){
-    columndiv =document.createElement('div');
-    columndiv.setAttribute("class" , "column")
+const button = document.getElementById('change-dimensions')
+button.addEventListener("click",askuser)
+function askuser(){
+    let dimensions = parseInt(prompt("Dimensions"))
+    if (dimensions > 0 && dimensions < 100){
+
     
-    for (let i = 0 ; i < 16 ; i++){
-        let rowdiv = document.createElement('div')
-        rowdiv.addEventListener("mouseover",
-        e => e.target.classList.add("hovered"))
-        rowdiv.setAttribute("class", "row")
-        columndiv.appendChild(rowdiv)
+
+    for (let i =0 ; i < dimensions; i++){
+        columndiv =document.createElement('div');
+        columndiv.setAttribute("class" , "column")
+    
+        for (let i = 0 ; i < dimensions ; i++){
+            let rowdiv = document.createElement('div')
+            rowdiv.addEventListener("mouseover",
+            e => e.target.classList.add("hovered"))
+            rowdiv.setAttribute("class", "row")
+            columndiv.appendChild(rowdiv)
+        }
+        container.appendChild(columndiv)
+
+        }
     }
-    container.appendChild(columndiv)
-
-
 }
