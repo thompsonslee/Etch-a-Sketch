dbe6b6ea572e092a68824387e2d9accfaca91477
thompsonslee <thompson.slee@hotmail.co.uk>
2023-01-31 15:01:14 +1100

    remove children in container when dimension button is clicked

--- script.js
+++ script.js
@@ -2,23 +2,24 @@ const container = document.querySelector('.container');
 const button = document.getElementById('change-dimensions')
 button.addEventListener("click",askuser)
 function askuser(){
+    container.innerHTML =""
     let dimensions = parseInt(prompt("Dimensions"))
     if (dimensions > 0 && dimensions < 100){
 
     
 
-    for (let i =0 ; i < dimensions; i++){
-        columndiv =document.createElement('div');
-        columndiv.setAttribute("class" , "column")
+        for (let i =0 ; i < dimensions; i++){
+            columndiv =document.createElement('div');
+            columndiv.setAttribute("class" , "column")
     
-        for (let i = 0 ; i < dimensions ; i++){
-            let rowdiv = document.createElement('div')
-            rowdiv.addEventListener("mouseover",
-            e => e.target.classList.add("hovered"))
-            rowdiv.setAttribute("class", "row")
-            columndiv.appendChild(rowdiv)
-        }
-        container.appendChild(columndiv)
+            for (let i = 0 ; i < dimensions ; i++){
+                let rowdiv = document.createElement('div')
+                rowdiv.addEventListener("mouseover",
+                e => e.target.classList.add("hovered"))
+                rowdiv.setAttribute("class", "row")
+                columndiv.appendChild(rowdiv)
+            }
+            container.appendChild(columndiv)
 
         }
     }
